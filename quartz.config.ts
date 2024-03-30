d074fd8a9aba74085cc445d9007eddb3ca364984
Mustafa Hussain <mustafa.hussain93@gmail.com>
2024-03-30 02:02:26 +0100

    update domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "zettelkasten.mhussain.net",
+    baseUrl: "mhussain.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
