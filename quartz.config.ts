96a4989070b7cd852c192c0142e1ebb48183bcbb
Mustafa Hussain <mustafa.hussain93@gmail.com>
2024-08-14 08:36:13 +0200

    sync 🔄

--- quartz.config.ts
+++ quartz.config.ts
@@ -79,6 +79,7 @@ const config: QuartzConfig = {
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: false,
+        
       }),
       Plugin.Assets(),
       Plugin.Static(),
